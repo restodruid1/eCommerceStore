5cfb9b365b86904ceaf1a19209acbadb95476e65
restodruid1 <106450663+restodruid1@users.noreply.github.com>
2025-11-01 22:26:02 -0700

    product container fixes

--- eCommerce_App/src/components/Products/Product.tsx
+++ eCommerce_App/src/components/Products/Product.tsx
@@ -10,18 +10,12 @@ interface ProductProps {
 export function Product ({ data, pageUrl }:ProductProps) {
     
     return (
-            <div>       
-            {/* <FaSackDollar style={{width:"100px", height:"200px", flex:"auto",margin: "5%", cursor:"pointer", backgroundColor:"lightgray", maxWidth:"100px"}}/> */}
-            <Link to={`/${pageUrl}/${data.id}/${data.name}`}><img src={`${data.url}`} style={{width:"100px", height:"200px", flex:"auto",margin: "5%", cursor:"pointer", backgroundColor:"lightgray", maxWidth:"100px"}}/></Link>
-            <ol>
-                <li>{data.id}</li> 
-                <li>{data.name}</li> 
-                <li>{data.quantity}</li> 
-                <li>{data.weight}</li> 
-                <li>{data.price}</li> 
-                <li>{data.description}</li>
-                <li>{data.created_at}</li>
-            </ol> 
+            <div className="productItems">
+                <Link to={`/${pageUrl}/${data.id}/${data.name}`}><img src={`${data.url}`} className='productImages'/></Link>
+                <div style={{display:"flex", flexDirection:"column", alignContent:"center", justifyContent:"center"}}>
+                    <h3 style={{textAlign:"center"}}>{data.name}</h3>
+                    <p style={{textAlign:"center"}}>${data.price}</p>
+                </div> 
             </div>
     )
 }
