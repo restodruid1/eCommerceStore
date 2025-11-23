797c0b95cfa6c50af739d97241d28f2f2f275d3a
restodruid1 <106450663+restodruid1@users.noreply.github.com>
2025-11-22 17:05:13 -0800

    admin page

---
+++ backend/src/admin.ts
@@ -0,0 +1,30 @@
+import { Router } from 'express';
+import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { requireAdmin } from './Middleware.js';
+
+const router = Router();
+
+router.post('/admin/login', (req: Request ,res:Response) => {
+    const { username, password } = req.body;
+    const JWT_SECRET = process.env.JWTSECRET;
+
+    if (username !== process.env.ADMIN_USERNAME || password !== process.env.ADMIN_PASSWORD) {
+        return res.status(401).json({ error: "Invalid credentials" });
+    }
+    
+    if (JWT_SECRET === undefined) return res.status(501).json({ error: "Server error" });
+    const token = jwt.sign({ role: "admin" }, JWT_SECRET!, { expiresIn: "1h" });
+
+    res.json({ message: "Logged in", jwt: token });
+});
+
+router.post('/admin/page', requireAdmin, (req,res)=>{
+    console.log(req);
+
+
+    res.json({message: "valid"});
+})
+
+
+export default router;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,6 +5,7 @@ import checkoutsession from "./checkoutsession.js";
 import databasequeries from "./databaseroutes.js";
 import handleshipping from "./shipping.js";
 import checkoutreturn from "./checkoutreturn.js";
+import admin from "./admin.js";
 import Stripe from "stripe";
 
 export interface Item {
@@ -23,6 +24,7 @@ export const stripe = new Stripe(`${process.env.TEST_STRIPE_API}`);
 app.use(express.json());
 app.use(cors({
     origin: 'http://localhost:5173',
+    credentials: true,
     methods: ["GET", "POST"]
   }));
 
@@ -30,6 +32,7 @@ app.use('/create-checkout-session', checkoutsession);
 app.use('/products', databasequeries);
 app.use('/calculate-shipping-options', handleshipping);
 app.use('/session_status', checkoutreturn);
+app.use('/api', admin);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
