2196590c4b1cb1d81011360fa3a2b71eeb11ec68
restodruid1 <106450663+restodruid1@users.noreply.github.com>
2025-12-31 14:46:56 -0800

    db url render

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -5,12 +5,15 @@ dotenv.config();
 const { Pool } = pg;
  
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASS,
-    port: Number(process.env.DB_PORT) || 5432,
+  connectionString: process.env.DB_URL,
   });
+// const pool = new Pool({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASS,
+//     port: Number(process.env.DB_PORT) || 5432,
+//   });
 
 // DO NOT USE POOL IF A TRANSACTION IS NEEDED i.e. A SERIES OF QUERIES WHERE ALL MUST BE SUCCESSFUL OR NONE ARE
 // USE THE PG CLIENT FOR TRANSACTIONS. POOL IS MORE EFFICIENT IS OTHER CASES
